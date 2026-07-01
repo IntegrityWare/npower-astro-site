fabdd7d9007241ab2c7a122f4b121d29368cc552
Gary Crocker <gcrocker61@gmail.com>
2026-07-01 09:25:28 +0000

    File changes

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -8,7 +8,7 @@ export default function Technology() {
   return (
     <div>
       <PageHero
-        title="Solids# Technology"
+        title="Solids#"
         subtitle="The advanced geometry kernel powering every Power Surfacing product."
         breadcrumbs={[{ label: "Technology" }]}
         image={IMAGES.cyborg3d} />
